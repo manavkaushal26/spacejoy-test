3f4eab76e0b94e60c30198b88f95b92e25d85cd0
Soham Bhaumik <soham.bhaumik@spacejoy.com>
2022-01-24 13:12:33 +0530

    added listener

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const SignupPage = () => {
+  useEffect(() => {
+    window.onmessage = (event) => {
+      console.log('event recevied', event);
+    };
+  }, []);
+
   return (
     <div className="h-screen">
       <iframe src="https://auth.spacejoy.com/" className="h-full w-full" />
