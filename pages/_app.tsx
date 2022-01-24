cc23408a65bc69d284d75ad0f0589233571b2586
sannair96 <sandeep.nair@spacejoy.com>
2022-01-24 19:07:31 +0530

    remove session provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import CommonSEO from '@components/Shared/SEO/DefaultSeo';
 import { Provider, useCreateStore } from '@lib/store';
 import AuthProvider from '@store/AuthProvider';
 import ShopFilterContextProvider from '@store/ShopFilterContext';
-import { SessionProvider } from 'next-auth/react';
 import React from 'react';
 import { Toaster } from 'react-hot-toast';
 import { ThemeProvider } from 'styled-components';
@@ -10,27 +9,26 @@ import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps: { session, ...pageProps } }): React.ReactElement => {
   const createStore = useCreateStore(pageProps.initialZustandState || {});
-  console.log('session', session);
 
   return (
     <>
       <AuthProvider>
-        <SessionProvider session={session}>
-          <Provider createStore={createStore}>
-            <CommonSEO />
-            <ThemeProvider theme={{}}>
-              <ShopFilterContextProvider>
-                <Component {...pageProps} />
-                <Toaster
-                  position="bottom-center"
-                  toastOptions={{
-                    className: 'shadow-lg',
-                  }}
-                />
-              </ShopFilterContextProvider>
-            </ThemeProvider>
-          </Provider>
-        </SessionProvider>
+        {/* <SessionProvider session={session}> */}
+        <Provider createStore={createStore}>
+          <CommonSEO />
+          <ThemeProvider theme={{}}>
+            <ShopFilterContextProvider>
+              <Component {...pageProps} />
+              <Toaster
+                position="bottom-center"
+                toastOptions={{
+                  className: 'shadow-lg',
+                }}
+              />
+            </ShopFilterContextProvider>
+          </ThemeProvider>
+        </Provider>
+        {/* </SessionProvider> */}
       </AuthProvider>
     </>
   );
