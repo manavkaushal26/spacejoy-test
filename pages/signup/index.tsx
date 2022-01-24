693ed929b0f37f06efbd776a8029dab4f2179864
Soham Bhaumik <soham.bhaumik@spacejoy.com>
2022-01-24 13:08:25 +0530

    added signup iframe

---
+++ pages/signup/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignupPage = () => {
+  return (
+    <div className="h-screen">
+      <iframe src="https://auth.spacejoy.com/" className="h-full w-full" />
+    </div>
+  );
+};
+
+export default SignupPage;
