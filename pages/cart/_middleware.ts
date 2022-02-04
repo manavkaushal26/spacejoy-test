144ff7f7bdf836494946841ef91740db6b39b414
Soham Bhaumik <soham.bhaumik@spacejoy.com>
2022-02-04 13:42:36 +0530

    WIP auth 1

--- pages/cart/_middleware.ts
+++ pages/cart/_middleware.ts
@@ -7,17 +7,15 @@ export function middleware(req: NextRequest) {
 
   const basicAuth = req.cookies['token'];
 
-
-
   if (basicAuth) {
     return NextResponse.next().cookie('basic-auth', basicAuth);
   }
 
-
-  return new Response('Auth required', {
-    status: 401,
-    headers: {
-      'WWW-Authenticate': 'Basic realm="Secure Area"',
-    },
-  })
+  return NextResponse.redirect('/unauthorised');
+  // return new Response('Auth required', {
+  //   status: 401,
+  //   headers: {
+  //     'WWW-Authenticate': 'Basic realm="Secure Area"',
+  //   },
+  // })
 }
