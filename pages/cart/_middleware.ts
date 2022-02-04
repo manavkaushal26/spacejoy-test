36eceb46112e95dac13da5b046d9675a5f90108e
Soham Bhaumik <soham.bhaumik@spacejoy.com>
2022-02-04 08:45:29 +0530

    WIP removed nexturl checkl

--- pages/cart/_middleware.ts
+++ pages/cart/_middleware.ts
@@ -4,16 +4,12 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(req: NextRequest) {
-  console.log('request intercepted --------------', req.cookies, req.nextUrl.pathname);
-  NextResponse.cookie('req.nextUrl.pathname', req.nextUrl.pathname)
-  // Only rewrite requests to `/`, as _middleware on the `/pages` root will be executed in every request of the app.
-  if (req.nextUrl.pathname === '/cart') {
-    // Parse the cookie
-    const isAuthorised = req.cookies['token'] ? true : false;
-    const redirectTo = isAuthorised ? '/cart/cart' : '/unauthorised';
+  // Parse the cookie
+  const isAuthorised = req.cookies['token'] ? true : false;
+  const redirectTo = isAuthorised ? '/cart/cart' : '/unauthorised';
 
-    // Rewrite to the correct page
-    return NextResponse.rewrite(redirectTo).cookie("redirect_path", "cart");
-  }
+  // Rewrite to the correct page
+  return NextResponse.rewrite(redirectTo).cookie("redirect_path", "cart");
+
 
 }
