4e3e93cbbc7461f29970ce8bb4a161131908144e
Soham Bhaumik <soham.bhaumik@spacejoy.com>
2022-02-04 08:43:27 +0530

    WIP

--- pages/cart/_middleware.ts
+++ pages/cart/_middleware.ts
@@ -5,6 +5,7 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(req: NextRequest) {
   console.log('request intercepted --------------', req.cookies, req.nextUrl.pathname);
+  NextResponse.cookie('req.nextUrl.pathname', req.nextUrl.pathname)
   // Only rewrite requests to `/`, as _middleware on the `/pages` root will be executed in every request of the app.
   if (req.nextUrl.pathname === '/cart') {
     // Parse the cookie
@@ -14,4 +15,5 @@ export function middleware(req: NextRequest) {
     // Rewrite to the correct page
     return NextResponse.rewrite(redirectTo).cookie("redirect_path", "cart");
   }
+
 }
