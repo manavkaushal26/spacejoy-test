636aa2494c3a8251935626a81b3ed61f7ca37264
Manav Kaushal <manavkaushal756@gmail.com>
2022-02-07 21:45:45 +0530

    added promises in footer and updated explore ideas tab

--- components/Shared/CustomerStoriesNav.tsx
+++ components/Shared/CustomerStoriesNav.tsx
@@ -4,78 +4,81 @@ import React from 'react';
 
 const CustomerStoriesNav = () => {
   return (
-    <div className=" max-w-800 max-w-screen-xl">
+    <div className="max-w-screen-xl min-h-[80vh]">
       <div className="grid grid-cols-4 gap-10">
-        <div className="shadow-sm rounded-xl bg-green-50 h-full cursor-pointer hover:bg-green-100" onClick={()=>location.href="/interior-designs"}>
-          <div className="flex h-full p-4 flex-col">
-            <div className="text-left">
-              <Image
-                src="https://res.cloudinary.com/spacejoy/image/upload/v1643898253/spj-v2/3d-icons/Icon_qtem1z.png"
-                alt="No markups"
-                height={'50'}
-                width={'65'}
-                layout="intrinsic"
-              />
-            </div>
-            <div>
-              <h3 className="text-lg mt-2 text-green-700 font-poppins">Interior Designs</h3>
-              <p className="text-sm mb-2">Discover inspiration for your next design project</p>
-              <ArrowRightIcon className="h-4 w-4" />
-            </div>
+        <div
+          className="h-full shadow-sm cursor-pointer rounded-xl bg-green-50 hover:bg-green-100"
+          onClick={() => (location.href = `/interior-designs`)}
+        >
+          <div className="relative w-full">
+            <Image
+              src="https://res.cloudinary.com/spacejoy/image/upload/v1644242817/spj-v2/interior_designs_v5ctez.jpg"
+              alt="Interior Designs"
+              width={300}
+              height={280}
+              className="rounded-tl-xl rounded-tr-xl"
+            />
+          </div>
+          <div className="flex flex-col p-4">
+            <h3 className="text-xl font-poppins">Interior Designs</h3>
+            <ArrowRightIcon className="w-9 h-9 p-[9px] text-white bg-black rounded-full my-2" />
           </div>
         </div>
-        <div className="shadow-sm rounded-xl bg-red-50 h-full cursor-pointer hover:bg-red-100" onClick={()=>location.href="/collection"}>
-          <div className="flex h-full p-4 flex-col">
-            <div className="text-left">
-              <Image
-                src="https://res.cloudinary.com/spacejoy/image/upload/v1643898249/spj-v2/3d-icons/Group_rp4mis.png"
-                alt="No markups"
-                height={'50'}
-                width={'65'}
-                layout="intrinsic"
-              />
-            </div>
-            <div>
-              <h3 className="text-lg mt-2 text-red-700 font-poppins">Collection</h3>
-              <p className="text-sm mb-2">Explore ideas for every style and space</p>
-              <ArrowRightIcon className="h-4 w-4" />
-            </div>
+
+        <div
+          className="h-full shadow-sm cursor-pointer rounded-xl bg-red-50 hover:bg-red-100"
+          onClick={() => (location.href = `/collection`)}
+        >
+          <div className="relative w-full">
+            <Image
+              src="https://res.cloudinary.com/spacejoy/image/upload/v1644244135/spj-v2/collections_silwlq.jpg"
+              alt="Collection"
+              width={300}
+              height={280}
+              className="rounded-tl-xl rounded-tr-xl"
+            />
+          </div>
+          <div className="flex flex-col p-4">
+            <h3 className="text-xl font-poppins">Collection</h3>
+            <ArrowRightIcon className="w-9 h-9 p-[9px] text-white bg-black rounded-full my-2" />
           </div>
         </div>
-        <div className="shadow-sm rounded-xl bg-cyan-50 h-full cursor-pointer hover:bg-cyan-100" onClick={()=>location.href="https://www.spacejoy.com/interior-designs-blog"}>
-          <div className="flex h-full p-4 flex-col">
-            <div>
-              <Image
-                src="https://res.cloudinary.com/spacejoy/image/upload/v1643898245/spj-v2/3d-icons/Group_1_t66jhx.png"
-                alt="No markups"
-                height={'50'}
-                width={'50'}
-                layout="intrinsic"
-              />
-            </div>
-            <div>
-              <h3 className="text-lg mt-2 text-cyan-700 font-poppins">Design Matters</h3>
-              <p className="text-sm mb-2">Tips & Guides</p>
-              <ArrowRightIcon className="h-4 w-4" />
-            </div>
+
+        <div
+          className="h-full shadow-sm cursor-pointer rounded-xl bg-cyan-50 hover:bg-cyan-100"
+          onClick={() => (location.href = `https://www.spacejoy.com/interior-designs-blog`)}
+        >
+          <div className="relative w-full">
+            <Image
+              src="https://res.cloudinary.com/spacejoy/image/upload/v1644244318/spj-v2/tips_and_guides_suvngg.jpg"
+              alt="Tips and Guides"
+              width={300}
+              height={280}
+              className="rounded-tl-xl rounded-tr-xl"
+            />
+          </div>
+          <div className="flex flex-col p-4">
+            <h3 className="text-xl font-poppins">Tips &amp; Guides</h3>
+            <ArrowRightIcon className="w-9 h-9 p-[9px] text-white bg-black rounded-full my-2" />
           </div>
         </div>
-        <div className="shadow-sm rounded-xl bg-yellow-50 h-full cursor-pointer hover:bg-yellow-100" onClick={()=>location.href="https://www.spacejoy.com/customer-stories"}>
-          <div className="flex h-full p-4 flex-col ">
-            <div>
-              <Image
-                src="https://res.cloudinary.com/spacejoy/image/upload/v1643898240/spj-v2/3d-icons/Group_2_mbdyzg.png"
-                alt="No markups"
-                height={'50'}
-                width={'50'}
-                layout="intrinsic"
-              />
-            </div>
-            <div>
-              <h3 className="text-lg mt-2 text-yellow-700 font-poppins">Customer Stories</h3>
-              <p className="text-sm mb-2">100% Happiness Delivered</p>
-              <ArrowRightIcon className="h-4 w-4" />
-            </div>
+
+        <div
+          className="h-full shadow-sm cursor-pointer rounded-xl bg-yellow-50 hover:bg-yellow-100"
+          onClick={() => (location.href = `https://www.spacejoy.com/customer-stories`)}
+        >
+          <div className="relative w-full">
+            <Image
+              src="https://res.cloudinary.com/spacejoy/image/upload/v1644244341/spj-v2/customer_stories_cdytgy.jpg"
+              alt="Customer Stories"
+              width={300}
+              height={280}
+              className="rounded-tl-xl rounded-tr-xl"
+            />
+          </div>
+          <div className="flex flex-col p-4">
+            <h3 className="text-xl font-poppins">Customer Stories</h3>
+            <ArrowRightIcon className="w-9 h-9 p-[9px] text-white bg-black rounded-full my-2" />
           </div>
         </div>
       </div>
--- components/Shared/Footer.tsx
+++ components/Shared/Footer.tsx
@@ -2,6 +2,8 @@ import { company } from '@utils/config';
 import Link from 'next/link';
 import React from 'react';
 import SocialLinks from './Footer/SocialLinks';
+import Image from 'next/image';
+import spacejoyPromiseData from '@utils/Mocks/spacejoyPromises';
 
 const currentDate = new Date();
 const currentYear = currentDate.getFullYear();
@@ -9,57 +11,74 @@ const currentYear = currentDate.getFullYear();
 const Footer: React.FC = () => {
   return (
     <footer className="antialiased border-t border-gray-200">
-      <div className="container mx-auto px-4">
-        <div className="py-12 grid grid-cols-6 gap-4">
+      <div className="container px-4 mx-auto">
+        <div className="grid grid-cols-3 gap-6">
+          {spacejoyPromiseData.map((promise) => (
+            <div key={promise.title} className="mt-4">
+              <div className="relative flex flex-col py-5 space-x-3 bg-white rounded-lg justify-left lg:flex-row lg:items-start">
+                <div className="flex-shrink-0">
+                  <img className="w-16 h-16 mb-2 ml-3 lg:mb-0 lg:ml-0" src={promise.img} alt={promise.title} />
+                </div>
+                <div className="flex-1 min-w-0 ml-0 lg:ml-2">
+                  <span className="absolute inset-0" aria-hidden="true" />
+                  <p className="mb-1 text-sm font-semibold text-gray-900">{promise.title}</p>
+                  <p className="text-sm text-gray-500">{promise.description}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="grid grid-cols-6 gap-4 py-12">
           <div className="col-span-2 xl:w-3/4">
             <h3 className="mb-2">About Us</h3>
-            <p className="leading-relaxed text-sm text-gray-600">
+            <p className="text-sm leading-relaxed text-gray-600">
               Spacejoy is an online interior design studio. See your room designed in 3D curated with handpicked
               products that you shop right away from our platform.
             </p>
             <SocialLinks />
           </div>
           <div className="">
-            <h3 className="mb-2 px-1">Quick Links</h3>
+            <h3 className="px-1 mb-2">Quick Links</h3>
             <ul>
               <li>
                 <Link href="/pricing">
-                  <a className="px-1 text-sm text-gray-600 leading-7 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none">
+                  <a className="px-1 text-sm leading-7 text-gray-600 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none">
                     Pricing
                   </a>
                 </Link>
               </li>
               <li>
                 <Link href="/customer-stories">
-                  <a className="px-1 text-sm text-gray-600 leading-7 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none">
+                  <a className="px-1 text-sm leading-7 text-gray-600 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none">
                     Customer Stories
                   </a>
                 </Link>
               </li>
               <li>
                 <Link href="/interior-designs">
-                  <a className="px-1 text-sm text-gray-600 leading-7 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none">
+                  <a className="px-1 text-sm leading-7 text-gray-600 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none">
                     Interior Designs Ideas
                   </a>
                 </Link>
               </li>
               <li>
                 <Link href="/purchase-gift-card">
-                  <a className="px-1 text-sm text-gray-600 leading-7 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none">
+                  <a className="px-1 text-sm leading-7 text-gray-600 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none">
                     Gift Cards
                   </a>
                 </Link>
               </li>
               <li>
                 <Link href="/online-room-design">
-                  <a className="px-1 text-sm text-gray-600 leading-7 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none">
+                  <a className="px-1 text-sm leading-7 text-gray-600 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none">
                     Design Process
                   </a>
                 </Link>
               </li>
               <li>
                 <Link href="/online-interior-designers">
-                  <a className="px-1 text-sm text-gray-600 leading-7 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none">
+                  <a className="px-1 text-sm leading-7 text-gray-600 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none">
                     Meet The Team
                   </a>
                 </Link>
@@ -67,7 +86,7 @@ const Footer: React.FC = () => {
             </ul>
           </div>
           <div className="">
-            <h3 className="mb-2 px-1">Blog</h3>
+            <h3 className="px-1 mb-2">Blog</h3>
             <ul>
               <li>
                 <Link
@@ -75,7 +94,7 @@ const Footer: React.FC = () => {
                     pathname: '/interior-designs-blog',
                   }}
                 >
-                  <a className="px-1 text-sm text-gray-600 leading-7 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none">
+                  <a className="px-1 text-sm leading-7 text-gray-600 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none">
                     Online Interior Design Blog
                   </a>
                 </Link>
@@ -89,7 +108,7 @@ const Footer: React.FC = () => {
                   as="/interior-designs-blog/for-a-year-round-spring-feel-light-and-airy-furniture"
                 >
                   <a
-                    className="px-1 text-sm text-gray-600 leading-7 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none"
+                    className="px-1 text-sm leading-7 text-gray-600 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none"
                     href="/interior-designs-blog/for-a-year-round-spring-feel-light-and-airy-furniture"
                   >
                     Spring Furniture Ideas
@@ -105,7 +124,7 @@ const Footer: React.FC = () => {
                   as="/interior-designs-blog/13-clever-and-stylish-storage-ideas-for-any-room-in-your-home"
                 >
                   <a
-                    className="px-1 text-sm text-gray-600 leading-7 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none"
+                    className="px-1 text-sm leading-7 text-gray-600 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none"
                     href="/interior-designs-blog/13-clever-and-stylish-storage-ideas-for-any-room-in-your-home"
                   >
                     13 Clever Storage Ideas
@@ -121,7 +140,7 @@ const Footer: React.FC = () => {
                   as="/interior-designs-blog/gallery-wall-ideas-to-dress-up-your-walls"
                 >
                   <a
-                    className="px-1 text-sm text-gray-600 leading-7 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none"
+                    className="px-1 text-sm leading-7 text-gray-600 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none"
                     href="/interior-designs-blog/gallery-wall-ideas-to-dress-up-your-walls"
                   >
                     Gallery Wall Ideas
@@ -137,7 +156,7 @@ const Footer: React.FC = () => {
                   as="/interior-designs-blog/simple-diy-decor-ideas-to-ring-in-spring"
                 >
                   <a
-                    className="px-1 text-sm text-gray-600 leading-7 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none"
+                    className="px-1 text-sm leading-7 text-gray-600 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none"
                     href="/interior-designs-blog/simple-diy-decor-ideas-to-ring-in-spring"
                   >
                     DIY Spring Decor Ideas
@@ -153,7 +172,7 @@ const Footer: React.FC = () => {
                   as="/interior-designs-blog/colorful-wreath-ideas-that-go-beyond-the-front-door"
                 >
                   <a
-                    className="px-1 text-sm text-gray-600 leading-7 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none"
+                    className="px-1 text-sm leading-7 text-gray-600 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none"
                     href="/interior-designs-blog/colorful-wreath-ideas-that-go-beyond-the-front-door"
                   >
                     Spring Wreath Ideas
@@ -163,46 +182,46 @@ const Footer: React.FC = () => {
             </ul>
           </div>
           <div className="">
-            <h3 className="mb-2 px-1">Support</h3>
+            <h3 className="px-1 mb-2">Support</h3>
             <ul>
               <li>
                 <Link href="/help">
-                  <a className="px-1 text-sm text-gray-600 leading-7 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none">
+                  <a className="px-1 text-sm leading-7 text-gray-600 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none">
                     Help
                   </a>
                 </Link>
               </li>
               <li>
                 <Link href="/terms">
-                  <a className="px-1 text-sm text-gray-600 leading-7 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none">
+                  <a className="px-1 text-sm leading-7 text-gray-600 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none">
                     Privacy Policy
                   </a>
                 </Link>
               </li>
               <li>
                 <Link href="/terms">
-                  <a className="px-1 text-sm text-gray-600 leading-7 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none">
+                  <a className="px-1 text-sm leading-7 text-gray-600 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none">
                     Terms of Service
                   </a>
                 </Link>
               </li>
               <li>
                 <Link href="/cookies">
-                  <a className="px-1 text-sm text-gray-600 leading-7 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none">
+                  <a className="px-1 text-sm leading-7 text-gray-600 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none">
                     Cookie Statement
                   </a>
                 </Link>
               </li>
               <li>
                 <Link href="/refund-policy" as="/refund-policy">
-                  <a className="px-1 text-sm text-gray-600 leading-7 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none">
+                  <a className="px-1 text-sm leading-7 text-gray-600 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none">
                     Refund Policy
                   </a>
                 </Link>
               </li>
               <li>
                 <Link href="/sitemap" as="/sitemap">
-                  <a className="px-1 text-sm text-gray-600 leading-7 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none">
+                  <a className="px-1 text-sm leading-7 text-gray-600 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none">
                     Sitemap
                   </a>
                 </Link>
@@ -210,11 +229,11 @@ const Footer: React.FC = () => {
             </ul>
           </div>
           <div className="">
-            <h3 className="mb-2 px-1">Connect with us</h3>
+            <h3 className="px-1 mb-2">Connect with us</h3>
             <ul>
               <li>
                 <a
-                  className="px-1 text-sm text-gray-600 leading-7 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none"
+                  className="px-1 text-sm leading-7 text-gray-600 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none"
                   href={`tel:${company.phone.support}`}
                   target="_top"
                 >
@@ -223,7 +242,7 @@ const Footer: React.FC = () => {
               </li>
               <li>
                 <a
-                  className="px-1 text-sm text-gray-600 leading-7 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none"
+                  className="px-1 text-sm leading-7 text-gray-600 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none"
                   href={`mailto:${company.email.support}?Subject=Need%20Help`}
                   target="_top"
                 >
@@ -232,7 +251,7 @@ const Footer: React.FC = () => {
               </li>
               <li>
                 <a
-                  className="px-1 text-sm text-gray-600 leading-7 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none"
+                  className="px-1 text-sm leading-7 text-gray-600 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none"
                   href={`mailto:${company.email.support}?Subject=Partner%20With%20Spacejoy`}
                   target="_top"
                 >
@@ -241,7 +260,7 @@ const Footer: React.FC = () => {
               </li>
               <li>
                 <Link href="/career" as="/career">
-                  <a className="px-1 text-sm text-gray-600 leading-7 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none">
+                  <a className="px-1 text-sm leading-7 text-gray-600 rounded-md hover:text-red-500 focus:text-red-500 focus:ring-1 focus:ring-gray-500 focus:outline-none">
                     Careers
                   </a>
                 </Link>
@@ -249,8 +268,8 @@ const Footer: React.FC = () => {
             </ul>
           </div>
         </div>
-        <div className="text-center border-t border-gray-100 py-4">
-          <p className="text-xs text-gray-900 leading-7">
+        <div className="py-4 text-center border-t border-gray-100">
+          <p className="text-xs leading-7 text-gray-900">
             &copy; {currentYear} Neo Design Labs Inc. All rights reserved.
           </p>
         </div>
