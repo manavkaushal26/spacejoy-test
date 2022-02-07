2f45f0ecda348ee31997b613784a3aa62cfbf689
siddant <siddant.vardey@spacejoy.com>
2022-02-08 01:19:08 +0530

    updated help content and added react-html-parser

--- mocks/FeaturedData.tsx
+++ mocks/FeaturedData.tsx
@@ -19,14 +19,14 @@ const data = [
     id: 'feature-5',
     logo: 'https://res.cloudinary.com/spacejoy/image/upload/v1610014241/shared/Brand%20logos%2007-2021/publication_LOGO-07_odqvkc.svg',
   },
-  {
-    id: 'feature-6',
-    logo: 'https://res.cloudinary.com/spacejoy/image/upload/v1610014242/shared/Brand%20logos%2007-2021/publication_LOGO-04_bj5aqr.svg',
-  },
-  {
-    id: 'feature-7',
-    logo: 'https://res.cloudinary.com/spacejoy/image/upload/v1610014241/shared/Brand%20logos%2007-2021/publication_LOGO-07_odqvkc.svg',
-  },
+  // {
+  //   id: 'feature-6',
+  //   logo: 'https://res.cloudinary.com/spacejoy/image/upload/v1610014242/shared/Brand%20logos%2007-2021/publication_LOGO-04_bj5aqr.svg',
+  // },
+  // {
+  //   id: 'feature-7',
+  //   logo: 'https://res.cloudinary.com/spacejoy/image/upload/v1610014241/shared/Brand%20logos%2007-2021/publication_LOGO-07_odqvkc.svg',
+  // },
   {
     id: 'feature-8',
     logo: 'https://res.cloudinary.com/spacejoy/image/upload/fl_lossy,q_auto/v1610014241/shared/Brand%20logos%2007-2021/publication_LOGO-09_dodwjx.svg',
