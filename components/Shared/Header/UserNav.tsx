dc4183e1ce1fe22453b523c45823dcb38c4ae621
Soham Bhaumik <soham.bhaumik@spacejoy.com>
2022-02-24 14:21:46 +0530

    addded fix for missing menu

--- components/Shared/Header/UserNav.tsx
+++ components/Shared/Header/UserNav.tsx
@@ -3,7 +3,6 @@ import { Menu, Transition } from '@headlessui/react';
 import { ChevronDownIcon } from '@heroicons/react/outline';
 import { useSession } from '@store/AuthProvider';
 import { oldSpacejoyUrl } from '@utils/config';
-import Link from 'next/link';
 import React, { Fragment } from 'react';
 
 function NextLink(props) {
@@ -37,7 +36,7 @@ const UserNav = () => {
             leaveFrom="transform opacity-100 scale-100"
             leaveTo="transform opacity-0 scale-95"
           >
-            <Menu.Items className="absolute right-0 w-56 mt-6 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <Menu.Items className="fixed right-4 w-56 mt-6 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
               <div>
                 <Menu.Item>
                   {({ active }) => (
