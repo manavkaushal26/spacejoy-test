1bd51e7d498474ed06ffd94435d6f3e64c308f03
sannair96 <sandeep.nair@spacejoy.com>
2022-02-28 15:45:50 +0530

    Design cards changed

--- components/RoomSelection/DesignSetCardV2.tsx
+++ components/RoomSelection/DesignSetCardV2.tsx
@@ -1,6 +1,6 @@
 import { CollagesListInterface } from '@components/Collages/interface';
 import { blurredBgProduct } from '@public/images/bg-base-64';
-import { PushEvent } from '@utils/analyticsLogger';
+import { priceToLocaleString } from '@utils/helpers';
 import Image from 'next/image';
 import Link from 'next/link';
 import React, { useMemo } from 'react';
@@ -18,11 +18,6 @@ const DesignSetCardV2: React.FC<DesignSetCardV2Props> = ({ designData, large }) 
     ];
   }, [designData]);
 
-  const bgClass = {
-    aspectRatio: `${designData.background !== '' ? (large ? 'ar_2.1,c_fill' : 'ar_2.1,c_fill') : 'ar_1.67,c_mpad'}`,
-    objectFit: `${designData.background === '' ? `objectFit={'cover'}` : `objectFit={'contain'}`}`,
-  };
-
   return (
     <Link
       href={{
@@ -30,72 +25,68 @@ const DesignSetCardV2: React.FC<DesignSetCardV2Props> = ({ designData, large }) 
       }}
     >
       <a>
-        <div
-          className={`grid grid-cols-4 border transition-all hover:shadow-lg rounded-xl overflow-hidden group  ${
-            large
-              ? 'aspect-[16/7] lg:aspect-[16/6] xl:aspect-[16/5]'
-              : 'aspect-[16/14] lg:aspect-[16/10] xl:aspect-[16/8]'
-          }`}
-          onClick={() => {
-            PushEvent({
-              category: `Select Design Set Card`,
-              action: `Go to Design Set PDP Page | ${designData?._id}`,
-              label: `Explore Design Set`,
-            });
-          }}
-        >
-          <div className={`relative overflow-hidden ${large ? 'col-span-3' : 'col-span-3'}`}>
-            <div className="z-10 h-full">
-              <div className="relative h-full">
-                <Image
-                  loading="eager"
-                  className="z-10"
-                  src={`https://res.cloudinary.com/spacejoy/image/upload/e_trim/ar_1.67,c_mpad/${
-                    large ? 'w_1400' : 'w_800'
-                  },c_pad/f_auto,q_auto/${designData?.thumbnail}`}
-                  layout="fill"
-                  alt={designData?.collageId}
-                  objectFit={'contain'}
-                  placeholder="blur"
-                  blurDataURL={blurredBgProduct}
-                />
+        <div className="grid grid-cols-4 rounded-lg overflow-hidden border">
+          <div className="col-span-4">
+            {tagsInPills?.length > 0 && !large && (
+              <div className="flex gap-1 mt-1 mb-2 px-4 pt-4 pb-2">
+                {tagsInPills.map((tag, index) => (
+                  <span
+                    key={`${tag}-${index}`}
+                    className="px-2 py-1 text-xs capitalize bg-neutral-500 text-white  border rounded-full"
+                  >
+                    {tag}
+                  </span>
+                ))}
               </div>
-            </div>
-            {/* <Image
-              className="z-0"
-              src={`https://res.cloudinary.com/spacejoy/image/upload/q_auto,w_1000,f_auto/v1643871522/spj-v2/DIY/room-background_ukes2y.jpg`}
-              layout="fill"
-              alt={`${designData?.collageId} background`}
-              objectFit="cover"
-            /> */}
+            )}
           </div>
-          <div className={`bg-gray-100  ${large ? 'col-span-1' : 'col-span-1'} p-8 flex flex-col justify-between`}>
-            <div className="flex flex-col gap-2">
-              <h2 className={`capitalize text-lg  line-clamp-2 font-bold tracking-normal`}>
-                {/* Change line-clamp-2 to line-clamp-1 if there is more data to be displayed */}
-                {designData?.name.slice(0, -11)}
-              </h2>
-              {tagsInPills?.length > 0 && (
-                <div className="flex gap-1 mt-1 mb-1">
+          <div className={`${large ? 'col-span-3' : 'col-span-4'} px-4 ${large && 'py-4'}`}>
+            <div className={`relative ${large ? 'aspect-[2.32]' : 'aspect-[2]'} `}>
+              <Image
+                loading="eager"
+                className="z-10 rounded-lg overflow-hidden"
+                src={`https://res.cloudinary.com/spacejoy/image/upload/e_trim/${
+                  large ? 'ar_2.32' : 'ar_2'
+                },c_fill/f_auto,q_auto/${designData?.thumbnail}`}
+                layout="fill"
+                alt={designData?.collageId}
+                objectFit={'contain'}
+                placeholder="blur"
+                blurDataURL={blurredBgProduct}
+              />
+            </div>
+          </div>
+          <div className={`${large ? 'col-span-1' : 'col-span-4'}  p-4  `}>
+            <div className="flex flex-col justify-between h-full">
+              {tagsInPills?.length > 0 && large && (
+                <div className="flex gap-1 mt-1 mb-2">
                   {tagsInPills.map((tag, index) => (
                     <span
                       key={`${tag}-${index}`}
-                      className="px-2 py-1 text-xs capitalize bg-gray-300 border rounded-md"
+                      className="px-2 py-1 text-xs capitalize bg-neutral-500 text-white  border rounded-full"
                     >
                       {tag}
                     </span>
                   ))}
                 </div>
               )}
-              <p className="text-gray-600 line-clamp-2">{designData?.description}</p>
-            </div>
-            <div>
-              {/* <div className="mt-2 mb-4 text-xl font-bold">{priceToLocaleString(designData?.price)}</div> */}
+              <div>
+                <div className={'flex flex-col gap-2'}>
+                  <h2 className={`capitalize text-lg  line-clamp-2  font-bold tracking-normal`}>
+                    {/* Change line-clamp-2 to line-clamp-1 if there is more data to be displayed */}
+                    {designData?.name.slice(0, -11)}
+                  </h2>
+                  {designData?.description && <p className="text-gray-600 line-clamp-2">{designData?.description}</p>}
+                </div>
+                <div>
+                  <div className="mt-2 mb-4 text-xl font-bold">{priceToLocaleString(designData?.price)}</div>
 
-              <div className="flex gap-4">
-                <a className="px-4 py-2 transition-all border border-black rounded-lg group-hover:scale-105 hover:shadow-xl">
-                  Explore
-                </a>
+                  <div className="flex gap-4">
+                    <a className="px-4 py-2 transition-all border border-black rounded-lg group-hover:scale-105 hover:shadow-xl">
+                      Personalize
+                    </a>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
