b9a9c36a5d95cc820d807af87ef60cdcb5ced5be
Soham Bhaumik <soham.bhaumik@spacejoy.com>
2022-03-25 14:46:12 +0530

    set cache control header to 0 maxage

--- pages/product-view/[slug].tsx
+++ pages/product-view/[slug].tsx
@@ -869,7 +869,7 @@ export const getServerSideProps = async ({ params, res }) => {
   const { slug } = params;
   // const response = await fetcher({ endPoint: `/v1/assets/getAssetBySlug?slug=${slug}`, method: 'GET' });
   const response = await fetcher({ endPoint: `/v2/asset/${slug}`, method: 'GET' });
-
+  res.setHeader('Cache-Control', 'public, s-maxage=0');
   const { data, statusCode } = response;
 
   if (statusCode <= 300) {
@@ -880,13 +880,14 @@ export const getServerSideProps = async ({ params, res }) => {
           subcategory: { name: subCategoryName },
         },
       } = data;
-      res.writeHead(301, { location: `/shop?subcategory=${subCategoryName}&alternatives=true` });
-      // return {
-      //   redirect: {
-      //     destination: `/shop?subcategory=${subCategoryName}&alternatives=true`,
-      //     permanent: false,
-      //   },
-      // };
+
+      // res.writeHead(301, { location: `/shop?subcategory=${subCategoryName}&alternatives=true` });
+      return {
+        redirect: {
+          destination: `/shop?subcategory=${subCategoryName}&alternatives=true`,
+          permanent: false,
+        },
+      };
     }
 
     return {
