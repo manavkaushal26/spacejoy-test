c43bd02c84190fb9859459e3f52c66e271467fc3
Soham Bhaumik <soham.bhaumik@spacejoy.com>
2022-03-25 16:36:37 +0530

    revalidae

--- pages/product-view/[slug].tsx
+++ pages/product-view/[slug].tsx
@@ -869,7 +869,7 @@ export const getStaticProps = async ({ params, res }) => {
   const { slug } = params;
   // const response = await fetcher({ endPoint: `/v1/assets/getAssetBySlug?slug=${slug}`, method: 'GET' });
   const response = await fetcher({ endPoint: `/v2/asset/${slug}`, method: 'GET' });
-  res.setHeader('Cache-Control', 'no-store');
+  // res.setHeader('Cache-Control', 'no-store');
   const { data, statusCode } = response;
 
   if (statusCode <= 300) {
