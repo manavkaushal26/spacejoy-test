f9b04fea86cd524eb18a4dac2bd69b59696d11e8
siddantvardey <75243270+siddantvardey@users.noreply.github.com>
2022-04-10 20:27:55 +0530

    updated hero image on homepage (#183)

--- components/Home/Hero/v3.tsx
+++ components/Home/Hero/v3.tsx
@@ -18,7 +18,7 @@ const V3 = () => {
       <div className="absolute inset-0">
         <Image
           className="object-cover rounded-lg"
-          src="https://res.cloudinary.com/spacejoy/image/upload/v1648726729/web/homepage-v3/jonathan-borba-COzqEKjaxqo-unsplash_1_u6m2q2.jpg"
+          src={isScreenSmall ? "https://res.cloudinary.com/spacejoy/image/upload/v1644042492/web/homepage-v3/Website-design-set-92_wymkdg.jpg" : "https://res.cloudinary.com/spacejoy/image/upload/v1648726729/web/homepage-v3/jonathan-borba-COzqEKjaxqo-unsplash_1_u6m2q2.jpg"}
           alt="home banner"
           layout="fill"
           placeholder="blur"
@@ -29,7 +29,7 @@ const V3 = () => {
       <div className="flex flex-col min-h-[700px] justify-center items-center relative z-10">
         <div
           className={`bg-white bg-opacity-70 rounded-xl py-6 px-14 absolute sm:mx-0 sm:top-15 sm:right-10 opacity-2 space-y-4 ${
-            isScreenSmall ? 'bottom-10' : ''
+            isScreenSmall ? 'top-10' : ''
           }`}
         >
           <h1 className="mb-1 text-3xl leading-tight sm:leading-normal text-center md:text-4xl">
@@ -57,7 +57,7 @@ const V3 = () => {
               <a
                 target="_blank"
                 type="button"
-                className="group text-center overflow-hidden shadow-sm hover:shadow-lg text-base sm:text-lg text-white py-2 xl:py-3 px-4 xl:px-10 mt-4 rounded-xl bg-gray-900 tracking-wide focus:ml-0.5 focus:ring-1 focus:ring-offset-1 focus:ring-offset-white focus:ring-gray-400 focus:outline-none"
+                className="group text-center overflow-hidden shadow-sm hover:shadow-lg text-base sm:text-lg text-white py-3 xl:py-3 px-4 xl:px-10 mt-4 rounded-xl bg-gray-900 tracking-wide focus:ml-0.5 focus:ring-1 focus:ring-offset-1 focus:ring-offset-white focus:ring-gray-400 focus:outline-none"
               >
                 Start Your Project
               </a>
@@ -66,7 +66,7 @@ const V3 = () => {
               <a
                 target="_blank"
                 type="button"
-                className="group text-center overflow-hidden shadow-sm hover:shadow-lg text-base sm:text-lg text-black py-2 xl:py-3 px-4 xl:px-10 mt-4 rounded-xl bg-gray-100 tracking-wide focus:ml-0.5 focus:ring-1 focus:ring-offset-1 focus:ring-offset-white focus:ring-gray-400 focus:outline-none"
+                className="group text-center overflow-hidden shadow-sm hover:shadow-lg text-base sm:text-lg text-black py-3 xl:py-3 px-4 xl:px-10 mt-4 rounded-xl bg-gray-100 tracking-wide focus:ml-0.5 focus:ring-1 focus:ring-offset-1 focus:ring-offset-white focus:ring-gray-400 focus:outline-none"
               >
                 Explore Design Ideas
               </a>
