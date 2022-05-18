b48f593072b142450e5a42a1790ad902dcfb4908
Manav Kaushal <82047038+manavkaushal26@users.noreply.github.com>
2022-05-18 14:08:30 +0530

    homepage carousel second banner updated (#228)
    Co-authored-by: Manav Kaushal <manavkaushal756@gmail.com>

--- components/Home/Hero/v3.tsx
+++ components/Home/Hero/v3.tsx
@@ -20,7 +20,7 @@ const bannerImages = [
     src: 'https://res.cloudinary.com/spacejoy/image/upload/v1650949830/web/homepage-v3/Homepage_xwarfz.png',
   },
   {
-    src: 'https://res.cloudinary.com/spacejoy/image/upload/v1652697330/web/homev3/Summer_Sale-15_p8torx.jpg',
+    src: 'https://res.cloudinary.com/spacejoy/image/upload/v1652850522/web/homev3/Summer_Sale-09_qzwctv.jpg',
   },
   {
     src: 'https://res.cloudinary.com/spacejoy/image/upload/v1652697329/web/homev3/Summer_Sale-16_hkupnx.jpg',
