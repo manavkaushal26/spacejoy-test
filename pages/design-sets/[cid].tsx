7865465f672f96d0131012a1f26922900965d644
Manav Kaushal <82047038+manavkaushal26@users.noreply.github.com>
2022-05-30 16:43:36 +0530

    Feat/design sets meta (#236)
    * design set urls updated
    
    * removed logs
    
    Co-authored-by: Manav Kaushal <manavkaushal756@gmail.com>

--- pages/design-sets/[cid].tsx
+++ pages/design-sets/[cid].tsx
@@ -12,7 +12,7 @@ import { NavSelectContextProvider } from '@store/NavSelect';
 import { PlaygroundAssetsContextProvider } from '@store/PlaygroundAssets';
 import RecommendationsListContextProvider from '@store/RecommendationsList';
 import { SelectedIdContextProvider } from '@store/SelectedId';
-import { cloudinary } from '@utils/config';
+import { cloudinary, company } from '@utils/config';
 import mainCategories from '@utils/constants/DesignSets/mainCategory';
 import fetcher from '@utils/fetcher';
 import { onlyUnique } from '@utils/helpers';
@@ -21,6 +21,7 @@ import { NextPage } from 'next';
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 import React, { useMemo, useRef } from 'react';
 
 const PlaygroundWithNoSSR = dynamic(() => import('@components/Playground'), { ssr: false });
@@ -32,6 +33,7 @@ interface CollageViewProps {
 }
 
 const SingleCollageSet: NextPage<CollageViewProps> = ({ assets, collageData, groupedAssetList }): JSX.Element => {
+  const router = useRouter();
   const correctedCollageName = useMemo(() => {
     return collageData?.name?.split('-').join(' ').slice(0, -10);
   }, [collageData]);
@@ -58,6 +60,25 @@ const SingleCollageSet: NextPage<CollageViewProps> = ({ assets, collageData, gro
     <Layout>
       <Head>
         <title>{`${correctedCollageName?.[0]?.toLocaleUpperCase()}${correctedCollageName?.slice(1)}`} | Spacejoy</title>
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={`${company.url}${router.asPath}`} />
+        <meta
+          property="og:title"
+          content={`${correctedCollageName?.[0]?.toLocaleUpperCase()}${correctedCollageName?.slice(1)} | Spacejoy`}
+        />
+        <meta property="og:image" content={`${cloudinary.baseDeliveryURL}/${collageData.thumbnail}`} />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:url" content={`${company.url}${router.asPath}`} />
+        <meta
+          property="twitter:title"
+          content={`${correctedCollageName?.[0]?.toLocaleUpperCase()}${correctedCollageName?.slice(1)} | Spacejoy`}
+        />
+        <meta property="twitter:image" content={`${cloudinary.baseDeliveryURL}/${collageData.thumbnail}`} />
+
+        <link rel="canonical" href={`${company.url}${router.asPath.split('?')[0]}`} />
         <base href="/" />
       </Head>
       <Layout.Banner />
@@ -138,11 +159,13 @@ export default SingleCollageSet;
 
 export const getServerSideProps = async ({ params }) => {
   const { cid } = params;
-  if (cid) {
-    if (cid) {
+  const designId = cid && cid?.includes('-') ? cid?.split('-')?.pop() : cid;
+
+  if (designId) {
+    if (designId) {
       try {
         const collageRes = await fetcher({
-          endPoint: `/v1/collages/${cid}`,
+          endPoint: `/v1/collages/${designId}`,
           method: 'GET',
         });
         const { data = {}, statusCode } = collageRes;
@@ -151,116 +174,127 @@ export const getServerSideProps = async ({ params }) => {
         } = collageRes;
         const { category: { _id: categoryId = '' } = {} } = categoryMap || {};
         if (statusCode <= 301) {
-          const collageProductIds = data?.meta?.view?.map((product) => product?.product);
-          const productRes = await fetcher({
-            endPoint: '/v1/assets/getAssetsDetail',
-            body: {
-              assets: [...collageProductIds],
-              fields: [
-                'price',
-                'name',
-                'renderImages',
-                'retailer',
-                'dimension',
-                'meta',
-                'imageUrl',
-                'cdn',
-                'inStock',
-                'status',
-                'slug',
-              ],
-            },
-            method: 'POST',
-          });
-          const { data: productData = {}, statusCode: status } = productRes;
-          const isError = status < 300 ? false : true;
-          if (isError) throw new Error();
-
-          // Getting unique data during page build for SEO
-
-          const uniqueAssetList =
-            Object.keys(productData)
-              ?.filter(onlyUnique)
-              .filter((key) => key !== 'price') || [];
-
-          const mainCategoryList = mainCategories.map((value) => value.id);
-
-          const groupedAssetList = uniqueAssetList.reduce((acc, assetId) => {
-            const asset = productData[assetId];
-            if (asset.price > 0) {
-              if (!asset?.cdn) {
-                const renderedImagecdn = asset?.renderImages?.[0]?.cdn;
-                asset.cdn = renderedImagecdn || blurredBgProduct;
-              }
-              const category = asset?.meta?.category?._id;
-              const isMainCategory = mainCategoryList.includes(category);
-              const categoryEntry = acc[isMainCategory ? category : 'addOn'] || { assets: [], price: 0 };
-              categoryEntry.assets.push(asset);
-              categoryEntry.price += asset?.price;
-              acc[isMainCategory ? category : 'addOn'] = categoryEntry;
-            }
-
-            return acc;
-          }, {});
-
-          const collageDataWithProductData = {
-            type: 'collage',
-            id: cid,
-            data: data?.meta?.view?.map((object) => {
-              const {
-                translation: {
-                  x: { $numberDecimal: xCoord } = { $numberDecimal: '' },
-                  y: { $numberDecimal: yCoord } = { $numberDecimal: '' },
-                } = {},
-                scale: {
-                  height: { $numberDecimal: heightCoord = '' },
-                  width: { $numberDecimal: widthCoord = '' },
-                },
-                playgroundScale: {
-                  height: { $numberDecimal: actualHeightCoord = '' } = {},
-                  width: { $numberDecimal: actualWidthCoord = '' } = {},
-                } = {},
-                rotation = '0',
-                imgSrc,
-              } = object;
-
-              return {
-                x: parseFloat(xCoord),
-                y: parseFloat(yCoord),
-                height: parseFloat(heightCoord),
-                width: parseFloat(widthCoord),
-                assetId: object?.product,
-                rotationValue: rotation,
-                stitchedAssetImage: imgSrc,
-                count: 12,
-                ...(actualWidthCoord && { playgroundWidth: parseFloat(actualWidthCoord) }),
-                ...(actualHeightCoord && { playgroundHeight: parseFloat(actualHeightCoord) }),
-                id: `in-playground-asset-${Math.random()}`,
-                price: !isError ? productData?.[object?.product]?.price || 0 : 0,
-                displayPrice: !isError ? productData?.[object?.product]?.price || 0 : 0,
-                retailer: !isError ? productData?.[object?.product]?.retailer?.name || '' : '',
-                renderImages: !isError ? productData?.[object?.product]?.renderImages || [{ cdn: '' }] : [{ cdn: '' }],
-                name: !isError ? productData?.[object?.product]?.name || '' : '',
-                depth: !isError ? productData?.[object?.product]?.dimension?.depth || 0 : 0,
-                vertical: !isError ? productData?.[object?.product]?.meta?.vertical?.name || '' : '',
-              };
-            }),
-          };
-
-          return {
-            props: {
-              collageData: {
-                ...collageRes?.data,
-                ...collageDataWithProductData,
-                categoryId,
-                background,
-                bgType,
+          if (!cid.includes('-')) {
+            return {
+              redirect: {
+                permanent: true,
+                destination: `/design-sets/${collageRes?.data?.slug?.split('-')?.slice(0, -1).join('-')}-${designId}`,
               },
-              assets: productData || [],
-              groupedAssetList: groupedAssetList || {},
-            },
-            // revalidate: 1, //TODO: Recheck the doc Data Fetching
-          };
+            };
+          } else {
+            const collageProductIds = data?.meta?.view?.map((product) => product?.product);
+            const productRes = await fetcher({
+              endPoint: '/v1/assets/getAssetsDetail',
+              body: {
+                assets: [...collageProductIds],
+                fields: [
+                  'price',
+                  'name',
+                  'renderImages',
+                  'retailer',
+                  'dimension',
+                  'meta',
+                  'imageUrl',
+                  'cdn',
+                  'inStock',
+                  'status',
+                  'slug',
+                ],
+              },
+              method: 'POST',
+            });
+            const { data: productData = {}, statusCode: status } = productRes;
+            const isError = status < 300 ? false : true;
+            if (isError) throw new Error();
+
+            // Getting unique data during page build for SEO
+
+            const uniqueAssetList =
+              Object.keys(productData)
+                ?.filter(onlyUnique)
+                .filter((key) => key !== 'price') || [];
+
+            const mainCategoryList = mainCategories.map((value) => value.id);
+
+            const groupedAssetList = uniqueAssetList.reduce((acc, assetId) => {
+              const asset = productData[assetId];
+              if (asset.price > 0) {
+                if (!asset?.cdn) {
+                  const renderedImagecdn = asset?.renderImages?.[0]?.cdn;
+                  asset.cdn = renderedImagecdn || blurredBgProduct;
+                }
+                const category = asset?.meta?.category?._id;
+                const isMainCategory = mainCategoryList.includes(category);
+                const categoryEntry = acc[isMainCategory ? category : 'addOn'] || { assets: [], price: 0 };
+                categoryEntry.assets.push(asset);
+                categoryEntry.price += asset?.price;
+                acc[isMainCategory ? category : 'addOn'] = categoryEntry;
+              }
+
+              return acc;
+            }, {});
+
+            const collageDataWithProductData = {
+              type: 'collage',
+              id: designId,
+              data: data?.meta?.view?.map((object) => {
+                const {
+                  translation: {
+                    x: { $numberDecimal: xCoord } = { $numberDecimal: '' },
+                    y: { $numberDecimal: yCoord } = { $numberDecimal: '' },
+                  } = {},
+                  scale: {
+                    height: { $numberDecimal: heightCoord = '' },
+                    width: { $numberDecimal: widthCoord = '' },
+                  },
+                  playgroundScale: {
+                    height: { $numberDecimal: actualHeightCoord = '' } = {},
+                    width: { $numberDecimal: actualWidthCoord = '' } = {},
+                  } = {},
+                  rotation = '0',
+                  imgSrc,
+                } = object;
+
+                return {
+                  x: parseFloat(xCoord),
+                  y: parseFloat(yCoord),
+                  height: parseFloat(heightCoord),
+                  width: parseFloat(widthCoord),
+                  assetId: object?.product,
+                  rotationValue: rotation,
+                  stitchedAssetImage: imgSrc,
+                  count: 12,
+                  ...(actualWidthCoord && { playgroundWidth: parseFloat(actualWidthCoord) }),
+                  ...(actualHeightCoord && { playgroundHeight: parseFloat(actualHeightCoord) }),
+                  id: `in-playground-asset-${Math.random()}`,
+                  price: !isError ? productData?.[object?.product]?.price || 0 : 0,
+                  displayPrice: !isError ? productData?.[object?.product]?.price || 0 : 0,
+                  retailer: !isError ? productData?.[object?.product]?.retailer?.name || '' : '',
+                  renderImages: !isError
+                    ? productData?.[object?.product]?.renderImages || [{ cdn: '' }]
+                    : [{ cdn: '' }],
+                  name: !isError ? productData?.[object?.product]?.name || '' : '',
+                  depth: !isError ? productData?.[object?.product]?.dimension?.depth || 0 : 0,
+                  vertical: !isError ? productData?.[object?.product]?.meta?.vertical?.name || '' : '',
+                };
+              }),
+            };
+
+            return {
+              props: {
+                collageData: {
+                  ...collageRes?.data,
+                  ...collageDataWithProductData,
+                  categoryId,
+                  background,
+                  bgType,
+                },
+                assets: productData || [],
+                groupedAssetList: groupedAssetList || {},
+              },
+              // revalidate: 1, //TODO: Recheck the doc Data Fetching
+            };
+          }
         } else {
           throw new Error(statusCode);
         }
