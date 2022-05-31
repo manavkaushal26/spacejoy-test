da603a85d28fecf2584eed6bc36e7810d748a3a4
Manav Kaushal <82047038+manavkaushal26@users.noreply.github.com>
2022-05-31 19:20:04 +0530

    home and shop banners updated (#238)
    Co-authored-by: Manav Kaushal <manavkaushal756@gmail.com>

--- components/EcommercePage/HeroCarousel.tsx
+++ components/EcommercePage/HeroCarousel.tsx
@@ -18,17 +18,17 @@ const data = [
   {
     id: 1,
     imgSrcMob:
-      'https://res.cloudinary.com/spacejoy/image/upload/fl_lossy,q_auto/w_800/v1653628443/web/furniture-decor-shop/Get_Ready_For_Memorial_Day_Sale-27_wovoci.jpg',
+      'https://res.cloudinary.com/spacejoy/image/upload/fl_lossy,q_auto/w_800/v1653994567/web/furniture-decor-shop/Memorial_Day_Flash_Sale-36_uragex.jpg',
     imgSrcDesk:
-      'https://res.cloudinary.com/spacejoy/image/upload/fl_lossy,q_auto/v1653628441/web/furniture-decor-shop/Get_Ready_For_Memorial_Day_Sale-26_firyiu.jpg',
+      'https://res.cloudinary.com/spacejoy/image/upload/fl_lossy,q_auto/v1653994572/web/furniture-decor-shop/Memorial_Day_Flash_Sale-35_pvur0t.jpg',
     href: '/shop',
   },
   {
     id: 2,
     imgSrcMob:
-      'https://res.cloudinary.com/spacejoy/image/upload/fl_lossy,q_auto/w_800/v1653909374/web/furniture-decor-shop/V2/Memorial_Day_Flash_Sale-07_copy_gphfaz.jpg',
+      'https://res.cloudinary.com/spacejoy/image/upload/fl_lossy,q_auto/w_800/v1653994466/web/furniture-decor-shop/Memorial_Day_Flash_Sale-28_llbtni.jpg',
     imgSrcDesk:
-      'https://res.cloudinary.com/spacejoy/image/upload/fl_lossy,q_auto/v1653909372/web/furniture-decor-shop/V2/Memorial_Day_Flash_Sale-06_ez49o7.jpg',
+      'https://res.cloudinary.com/spacejoy/image/upload/fl_lossy,q_auto/v1653994467/web/furniture-decor-shop/Memorial_Day_Flash_Sale-27_txl94n.jpg',
     href: `${oldSpacejoyUrl}/new-project`,
   },
   {
