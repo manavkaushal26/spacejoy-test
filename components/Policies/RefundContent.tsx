ec7a22ac2f1119e60dc41eaca8f9e66faaeac99f
souravspacejoy <126777382+souravspacejoy@users.noreply.github.com>
2023-06-21 23:05:36 +0530

    update text

--- components/Policies/RefundContent.tsx
+++ components/Policies/RefundContent.tsx
@@ -49,6 +49,8 @@ const RefundContent: React.FC = () => {
           Full refunds only apply to projects that have not been started, or projects that have been delivered within
           the last 30 days. If the project was delivered over 30 days ago, we can only offer a partial refund (to be
           determined by the claims team).
+          <br/>
+          <h1> Partial refunds will only be considered for those projects that have been delivered within 4 months of the project start date. After 4 months, no design refunds will be issued.</h1>
         </p>
       </div>
       <div className="mt-6">
