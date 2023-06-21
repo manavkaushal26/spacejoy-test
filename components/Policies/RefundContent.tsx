4128918ccb9880088f3787b2c389c41eacfdd623
souravspacejoy <126777382+souravspacejoy@users.noreply.github.com>
2023-06-21 23:09:36 +0530

    add css

--- components/Policies/RefundContent.tsx
+++ components/Policies/RefundContent.tsx
@@ -50,7 +50,7 @@ const RefundContent: React.FC = () => {
           the last 30 days. If the project was delivered over 30 days ago, we can only offer a partial refund (to be
           determined by the claims team).
           <br/>
-          <h1> Partial refunds will only be considered for those projects that have been delivered within 4 months of the project start date. After 4 months, no design refunds will be issued.</h1>
+          <p className='refund-text-add'> Partial refunds will only be considered for those projects that have been delivered within 4 months of the project start date. After 4 months, no design refunds will be issued.</p>
         </p>
       </div>
       <div className="mt-6">
