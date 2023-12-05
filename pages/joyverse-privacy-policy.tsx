2d394346d0fea0d19fd4b3c425a4e978c2aa34b2
Manav Kaushal <82047038+manavkaushal26@users.noreply.github.com>
2023-12-06 02:03:40 +0530

    updated joyverse privacy policy (#291)
    Co-authored-by: Manav Kaushal <manavkaushal756@gmail.com>

--- pages/joyverse-privacy-policy.tsx
+++ pages/joyverse-privacy-policy.tsx
@@ -10,17 +10,17 @@ const JoyversePrivacyPolicy = (props: Props) => {
   return (
     <Layout>
       <Head>
-        <title key="title">Joyverse Privacy Policy</title>
+        <title key="title">Privacy Policy</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/* <Layout.Banner />
       <Layout.Header /> */}
       <Layout.Body>
         <div className="container px-4 mx-auto my-16 antialiased text-black break-words mb-28 joyverse-privacy-policy">
-          <SectionTitle title="Joyverse Privacy Policy" feature={`Last updated: Tuesday, December 5, 2023`} />
+          <SectionTitle title="Privacy Policy" feature={`Last updated: Tuesday, December 5, 2023`} />
           <p>
-            This privacy notice for Joyverse Innovation Labs Inc. (&quot;Company,&quot; &quot;we,&quot; &quot;us,&quot;
-            or &quot;our&quot;), describes how and why we might collect, store, use, and/or share (&quot;process&quot;)
+            This privacy notice for Neo Design Labs Inc. (&quot;Company,&quot; &quot;we,&quot; &quot;us,&quot; or
+            &quot;our&quot;), describes how and why we might collect, store, use, and/or share (&quot;process&quot;)
             your information when you download and use our Joyverse virtual reality-based application (the
             &quot;App&quot; and, with the Website, the &quot;Services&quot;).
           </p>
@@ -38,7 +38,7 @@ const JoyversePrivacyPolicy = (props: Props) => {
             <em>
               This summary provides key points from our privacy notice, but you can find out more details about any of
               these topics by clicking the link following each key point or by using our table of contents below to find
-              the section you are looking for. You can also click{' '}
+              the section you are looking for. You can also click
               <a href="#table-of-contents" className="link">
                 here
               </a>{' '}
@@ -47,8 +47,8 @@ const JoyversePrivacyPolicy = (props: Props) => {
           </p>
           <p>
             What personal information do we process? When you visit, use, or navigate our Services, we may process
-            personal information depending on how you interact with ProYuga® Advanced Technologies Ltd. and the
-            Services, the choices you make, and the products and features you use. Click{' '}
+            personal information depending on how you interact with Neo Design Labs inc. and the Services, the choices
+            you make, and the products and features you use. Click
             <a href="#what-information-do-we-collect" className="link">
               here
             </a>{' '}
@@ -59,7 +59,7 @@ const JoyversePrivacyPolicy = (props: Props) => {
             How do we process your information? We process your information to provide, improve, and administer our
             Services, communicate with you, for security and fraud prevention, and to comply with law. We may also
             process your information for other purposes with your consent. We process your information only when we have
-            a valid legal reason to do so. Click{' '}
+            a valid legal reason to do so. Click
             <a href="#how-do-we-process-your-information" className="link">
               here
             </a>{' '}
@@ -77,8 +77,8 @@ const JoyversePrivacyPolicy = (props: Props) => {
             How do we keep your information safe? We have organizational and technical processes and procedures in place
             to protect your personal information. However, no electronic transmission over the internet or information
             storage technology can be guaranteed to be 100% secure, so we cannot promise or guarantee that hackers,
-            cybercriminals, or other unauthorized third parties will not be able to defeat our security and improperly
-            collect, access, steal, or modify your information. Click{' '}
+            cybercriminals, or other unauthorized third parties will not be able to defeat our security and
+            improperly collect, access, steal, or modify your information. Click{' '}
             <a href="#how-do-we-keep-your-information-safe" className="link">
               here
             </a>{' '}
@@ -93,13 +93,11 @@ const JoyversePrivacyPolicy = (props: Props) => {
             to learn more.
           </p>
           <p>
-            How do you exercise your rights? The easiest way to exercise your rights is by filling out our data subject
-            request form available{' '}
-            <a href="" className="link">
-              here
+            How do you exercise your rights? The easiest way to exercise your rights is by submitting request to{' '}
+            <a href="mailto:hello@joyverse.club" className="link">
+              hello@joyverse.club
             </a>
-            , or by contacting us. We will consider and act upon any request in accordance with applicable data
-            protection laws.
+            . We will consider and act upon any request in accordance with applicable data protection laws.
           </p>
           <h3>
             <span id="table-of-contents">TABLE OF CONTENTS</span>
@@ -187,7 +185,7 @@ const JoyversePrivacyPolicy = (props: Props) => {
           </p>
           <p>
             We collect the following data about you: Identification data (user ids, name and profile pic); Technical
-            data and Game Play data(refer below for more details).
+            data and App data (refer below for more details).
           </p>
           <p>
             Social Media Login Data. We may provide you with the option to Login with us using your facebook account. If
@@ -235,8 +233,8 @@ const JoyversePrivacyPolicy = (props: Props) => {
           <ul>
             <li>
               <p>
-                <em>Technical Data and App Data.</em> When you play or use our Game and its Services, we collect
-                necessary data like headset and controller position so that the game &amp; its services are functional,
+                <em>Technical Data and App Data.</em> When you play or use our App and its Services, we collect
+                necessary data like headset and controller position so that the App &amp; its services are functional,
                 including leaderboards.
               </p>
             </li>
@@ -417,12 +415,12 @@ const JoyversePrivacyPolicy = (props: Props) => {
             <li>
               With Other Players. In order to support the multiplayer activities within our Games, such as adding
               friends, inviting people to play, or accepting challenge invites, we may share your information with other
-              players including your username (encrypted form unless in the game), play history, and scores.
+              players including your username (encrypted form unless in the App), play history, and scores.
             </li>
             <li>
               Service Providers. Your personal data are also processed by Service Providers, which ensures the creation
-              and management of statistics, error reports concerning the game and its services as well as to offer
-              in-game services.
+              and management of statistics, error reports concerning the App and its services as well as to offer in-App
+              services.
             </li>
             <li>
               As Required by Law. We may disclose your information if we believe that the disclosure is required by law,
@@ -616,9 +614,9 @@ const JoyversePrivacyPolicy = (props: Props) => {
             </a>{' '}
             or by post to:
           </p>
-          <address>Joyverse Innovation Labs Inc.</address>
-          <address>900 Pepper Tree Ln, 9208</address>
-          <address>Santa Clara, CA 95051</address>
+          <address>Neo Design Labs Inc.</address>
+          <address>1450 2nd Street, 155</address>
+          <address>Santa Monica, CA 90401</address>
           <h3 id="how-can-you-review-update-delete-data-collected">
             14. HOW CAN YOU REVIEW, UPDATE, OR DELETE THE DATA WE COLLECT FROM YOU?
           </h3>
@@ -631,7 +629,7 @@ const JoyversePrivacyPolicy = (props: Props) => {
           </p>
         </div>
       </Layout.Body>
-      <Layout.Footer />
+      {/* <Layout.Footer /> */}
     </Layout>
   );
 };
