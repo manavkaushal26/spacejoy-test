9758ca08ab64583f75f50e9e9e5054602ec14b2e
Manav Kaushal <82047038+manavkaushal26@users.noreply.github.com>
2024-01-25 18:38:39 +0530

    updated banner in interior designs slug (#293)
    Co-authored-by: Manav Kaushal <manavkaushal756@gmail.com>

--- components/Collection/Banner.tsx
+++ components/Collection/Banner.tsx
@@ -1,6 +1,6 @@
 import { ArrowNarrowRightIcon } from '@heroicons/react/outline';
 import useBoolean from '@hooks/useBoolean';
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import { Tween } from 'react-gsap';
 
 const months = [
@@ -28,28 +28,38 @@ interface BannerProps {
 }
 
 const CollectionBanner: React.FC<BannerProps> = ({ data }) => {
+  const initialLengthToShow = 275; // Adjust the initial length to show
+  const [showFullParagraph, setShowFullParagraph] = useState(false);
+
+  const toggleParagraph = () => {
+    setShowFullParagraph((prev) => !prev);
+  };
+
   const date = new Date(data?.publishedDate || '');
   const formattedDate = `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
-  const { value, toggle } = useBoolean(false);
+
+  const displayedText = useMemo(() => {
+    return showFullParagraph ? (
+      data?.description.split(/\r?\n/).map((str) => (
+        <p key={str} className="mt-4 text-gray-700">
+          {str}
+        </p>
+      ))
+    ) : (
+      <p className="inline">{data?.description.slice(0, initialLengthToShow)}... </p>
+    );
+  }, [data?.description, showFullParagraph]);
 
   return (
-    <div className="container mx-auto px-4 pt-8">
+    <div className="container px-4 pt-8 mx-auto">
       <div className="flex items-center xl:space-x-10 2xl:space-x-20">
         <div className="max-w-4xl">
           <Tween from={{ opacity: 0, x: 20 }} to={{ opacity: 1, x: 0 }} duration={1} stagger={0.5}>
-            <h1 className="text-2xl lg:text-5xl lg:leading-snug text-gray-900 mb-4">{data?.name}</h1>
+            <h1 className="mb-4 text-2xl text-gray-900 lg:text-5xl lg:leading-snug">{data?.name}</h1>
             <div className="mt-4 mb-8 text-sm text-gray-600">
-              <p className={`${!value && 'line-clamp-3'} text-gray-700`}>
-                {data?.description.split(/\r?\n/).map((str) => {
-                  return (
-                    <p key={str} className="mt-2">
-                      {str}
-                    </p>
-                  );
-                })}
-              </p>
-              <button className="my-1 text-gray-500" onClick={toggle}>
-                {!value ? '... read more' : 'hide'}
+              {displayedText}
+              <button className="text-[#F5296E]" onClick={toggleParagraph}>
+                {!showFullParagraph ? ' read more' : ' hide'}
               </button>
             </div>
           </Tween>
