bd21889fd87635a08415bc4ac5b303ad6f4af4b3
Manav Kaushal <manavkaushal756@gmail.com>
2024-07-01 15:50:22 +0530

    updated home page video

--- pages/index.tsx
+++ pages/index.tsx
@@ -277,7 +277,7 @@ export const Home = ({ isMobile, pricingData }): JSX.Element => {
           <div className="container px-4 mx-auto mt-16 mb-6 xl:px-20 sm:mt-32 sm:mb-12">
             <video width="400" controls autoPlay className="w-full aspect-video" muted poster={homePagePoster}>
               <source
-                src="https://res.cloudinary.com/spacejoy/video/upload/v1655115548/Comp_1_bumvd8.mp4"
+                src="https://res.cloudinary.com/spacejoy/video/upload/v1719828761/spj-v2/homepage_video_compressed_hlom7a.mp4"
                 type="video/webm"
               />
               Your browser does not support HTML video.
