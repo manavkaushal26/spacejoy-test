12a60edfe48fb9123dd4480a694128cc7adc3022
Manav Kaushal <manavkaushal756@gmail.com>
2024-09-16 10:49:02 +0530

    removed sale banners

--- components/Home/Hero/v3.tsx
+++ components/Home/Hero/v3.tsx
@@ -16,11 +16,11 @@ const responsive = {
   autoplaySpeed: 3000,
 };
 const bannerImages = [
-  {
-    src: 'https://res.cloudinary.com/spacejoy/image/upload/fl_lossy,q_100/w_1920,e_sharpen/v1726126050/spj-v2/Red_and_Blush_Pink_Flash_Sale_Discount_Animated_Instagram_Story_1_h14q4k.png',
-    mobileSrc: '',
-    link: '',
-  },
+  // {
+  //   src: 'https://res.cloudinary.com/spacejoy/image/upload/fl_lossy,q_100/w_1920,e_sharpen/v1726126050/spj-v2/Red_and_Blush_Pink_Flash_Sale_Discount_Animated_Instagram_Story_1_h14q4k.png',
+  //   mobileSrc: '',
+  //   link: '',
+  // },
   {
     src: 'https://res.cloudinary.com/spacejoy/image/upload/fl_lossy,q_auto/v1656677852/spj-v2/Summer_design_01-_Camera_1_1_-min_yz83hj.png',
     mobileSrc: '',
