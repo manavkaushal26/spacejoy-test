094ee8ece7d7948cece178f05a873f8732ad1b83
Manav Kaushal <manavkaushal756@gmail.com>
2024-11-19 16:26:33 +0530

    updated section heading component

--- components/EcommercePage/SectionHeading.tsx
+++ components/EcommercePage/SectionHeading.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 
-const SectionHeading: React.FC<{ title: string; subTitle?: string; noMargin?: boolean; center?: boolean }> = ({
-  title,
-  subTitle,
-  noMargin,
-  center,
-}) => {
+const SectionHeading: React.FC<{
+  preText?: string;
+  title: string | ReactNode;
+  subTitle?: string;
+  noMargin?: boolean;
+  center?: boolean;
+}> = ({ preText = '', title, subTitle = '', noMargin = false, center = false }) => {
   return (
     <div
       className={`w-full space-y-2 ${center ? 'text-center' : 'text-left'} ${noMargin ? 'mt-0 mb-0' : 'mt-16 mb-8'}`}
     >
+      {preText && (
+        <span className="border font-medium border-gray-500 py-0.5 px-1.5 rounded-full text-sm text-gray-500">
+          {preText}
+        </span>
+      )}
       <h2 className="text-3xl font-semibold capitalize lg:text-5xl">{title}</h2>
-      {subTitle && <p className="text-base text-zinc-500 lg:text-xl lg:mx-auto">{subTitle}</p>}
+      {subTitle && <p className="text-base text-gray-500 lg:text-xl lg:mx-auto">{subTitle}</p>}
     </div>
   );
 };
