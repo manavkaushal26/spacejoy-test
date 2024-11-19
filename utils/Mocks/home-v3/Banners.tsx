a104a1aafd6d0573ad6a63214f481f72a1d1db82
Manav Kaushal <manavkaushal756@gmail.com>
2024-11-19 16:26:54 +0530

    created banners section

---
+++ utils/Mocks/home-v3/Banners.tsx
@@ -0,0 +1,18 @@
+export const banners = [
+  {
+    src: 'https://res.cloudinary.com/spacejoy/image/upload/fl_lossy,q_auto,w_1000,ar_3:2,c_fill,g_center/v1656677852/spj-v2/Summer_design_01-_Camera_1_1_-min_yz83hj.png',
+    className: 'md:col-span-2',
+  },
+  {
+    src: 'https://res.cloudinary.com/spacejoy/image/upload/fl_lossy,q_auto,w_800,ar_3:2,c_fill,g_center/v1656677852/spj-v2/Summer_design_01-_Camera_1_1_-min_yz83hj.png',
+    className: 'md:col-span-1',
+  },
+  {
+    src: 'https://res.cloudinary.com/spacejoy/image/upload/fl_lossy,q_auto,w_800,ar_3:2,c_fill,g_center/v1656677852/spj-v2/Summer_design_01-_Camera_1_1_-min_yz83hj.png',
+    className: 'md:col-span-1',
+  },
+  {
+    src: 'https://res.cloudinary.com/spacejoy/image/upload/fl_lossy,q_auto,w_1000,ar_3:2,c_fill,g_center/v1656677852/spj-v2/Summer_design_01-_Camera_1_1_-min_yz83hj.png',
+    className: 'md:col-span-2',
+  },
+];
