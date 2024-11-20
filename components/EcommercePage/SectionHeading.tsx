bba2f0cb13937f38f30c809947219ec99b164e67
Manav Kaushal <manavkaushal756@gmail.com>
2024-11-20 15:39:14 +0530

    updated section heading

--- components/EcommercePage/SectionHeading.tsx
+++ components/EcommercePage/SectionHeading.tsx
@@ -1,3 +1,4 @@
+import Badge from '@components/Shared/Badge';
 import React, { ReactNode } from 'react';
 
 const SectionHeading: React.FC<{
@@ -11,11 +12,7 @@ const SectionHeading: React.FC<{
     <div
       className={`w-full space-y-2 ${center ? 'text-center' : 'text-left'} ${noMargin ? 'mt-0 mb-0' : 'mt-16 mb-8'}`}
     >
-      {preText && (
-        <span className="px-3 py-1 text-sm font-medium text-gray-500 border border-gray-500 rounded-full">
-          {preText}
-        </span>
-      )}
+      {preText && <Badge content={preText} />}
       <h2 className="text-3xl font-semibold capitalize lg:text-5xl">{title}</h2>
       {subTitle && <p className="text-base text-gray-500 lg:text-xl lg:mx-auto">{subTitle}</p>}
     </div>
