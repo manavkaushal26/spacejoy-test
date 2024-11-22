8e871122b7a8ec3d85044cdc47b061f742952efa
Manav Kaushal <manavkaushal756@gmail.com>
2024-11-22 12:00:16 +0530

    updated section

--- components/Home/v3/BeautifulSpaces.tsx
+++ components/Home/v3/BeautifulSpaces.tsx
@@ -12,7 +12,7 @@ const BeautifulSpaces = (props: Props) => {
   return (
     <>
       <div>
-        <MaxWidthContainer className="mt-0">
+        <MaxWidthContainer className="pt-0">
           <SectionHeading
             preText={
               <div className="p-2 mx-auto rounded-full bg-gradient-to-b from-orange-100 to-orange-300 w-fit">
@@ -49,13 +49,13 @@ const BeautifulSpaces = (props: Props) => {
                 title="Brera"
               />
             </div>
-            <div className="row-span-2 col-start-2 row-start-4">
+            <div className="col-start-2 row-span-2 row-start-4">
               <ImageCard
                 imgSrc="https://res.cloudinary.com/spacejoy/image/upload/v1732167831/spj-v2/home-v3/hero_bg_11_xyqqjo.jpg"
                 title="Alea Pro"
               />
             </div>
-            <div className="row-span-3 col-start-3 row-start-3">
+            <div className="col-start-3 row-span-3 row-start-3">
               <ImageCard
                 imgSrc="https://res.cloudinary.com/spacejoy/image/upload/v1732167831/spj-v2/home-v3/hero_bg_11_xyqqjo.jpg"
                 title="Nirnia"
@@ -91,12 +91,12 @@ const ImageCard = ({
           >
             <Image src={imgSrc} alt={title} layout="fill" objectFit="cover" />
           </div>
-          <div className="bg-gradient-to-b from-black/0 via-black/25 to-black absolute inset-0 text-white flex items-end">
+          <div className="absolute inset-0 flex items-end text-white bg-gradient-to-b from-black/0 via-black/25 to-black">
             <div className="flex items-center justify-between w-full p-6">
               <p className="text-3xl">{title}</p>
-              <div className="bg-white p-3 rounded-full">
+              <div className="p-3 bg-white rounded-full">
                 <ArrowRightIcon
-                  className="group-hover:-rotate-45 focus:-rotate-45 group-hover:scale-105 focus:scale-105 transition-all duration-300 w-6 h-6 text-black"
+                  className="w-6 h-6 text-black transition-all duration-300 group-hover:-rotate-45 focus:-rotate-45 group-hover:scale-105 focus:scale-105"
                   aria-hidden="true"
                 />
               </div>
