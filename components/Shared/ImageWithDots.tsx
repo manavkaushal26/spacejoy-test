1b9a16901084199aca8147509f3d383acbbdb841
Manav Kaushal <manavkaushal756@gmail.com>
2024-11-22 09:45:16 +0530

    updated lookbook section

--- components/Shared/ImageWithDots.tsx
+++ components/Shared/ImageWithDots.tsx
@@ -1,4 +1,4 @@
-import { BookmarkIcon } from '@heroicons/react/outline';
+import { ExternalLinkIcon } from '@heroicons/react/outline';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
@@ -42,20 +42,21 @@ const ImageWithDots: React.FC<ImageWithDotsProps> = ({ src, alt, width, height, 
             transform: 'translate(-50%, -50%)',
           }}
         >
-          <div className="relative w-5 h-5">
-            <div className="absolute inset-0 w-5 h-5 rounded-full border-[5px] border-spj-red/75 animate-pulse opacity-100 group-hover:animate-none transition-all duration-200" />
-            <div className="absolute inset-0 w-3 h-3 transition-transform duration-200 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-full md:group-hover:scale-125 top-1/2 left-1/2" />
+          <div className="relative w-6 h-6">
+            <div className="w-full h-full bg-white rounded-full flex items-center justify-center">
+              <div className="w-3 h-3 rounded-full bg-spj-red" />
+            </div>
           </div>
 
-          <span className="px-3 py-1 mt-2 text-sm font-semibold transition-all duration-200 translate-y-1 rounded-md shadow-md opacity-100 bg-white/75 md:opacity-0 md:group-hover:opacity-100 md:group-hover:translate-y-0">
-            {dot.label}
-          </span>
+          <span className="px-3 py-1 mt-2 text-sm font-semibold rounded-md shadow-md bg-white">{dot.label}</span>
         </div>
       ))}
       <div className="absolute bottom-0 left-0 font-medium text-white leading-[1] bg-black rounded-tr-[2rem] bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-30 p-6 min-w-[38rem] max-w-[40rem] w-full">
         <Link href={designData.url ?? '#'}>
           <a target="_blank" rel="noopener noreferrer" className="flex items-center space-x-2 hover:underline">
-            <BookmarkIcon className="w-5 h-5" /> <p className="text-2xl">{designData.name}</p>
+            {/* <BookmarkIcon className="w-5 h-5" /> */}
+            <p className="text-2xl">{designData.name}</p>
+            <ExternalLinkIcon className="w-5 h-5" />
           </a>
         </Link>
         <p className="text-base opacity-80">{designData.description}</p>
