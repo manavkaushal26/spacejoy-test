c8da5b3f03be43a349a8e0a802374fdf45af6a0e
Manav Kaushal <manavkaushal756@gmail.com>
2025-02-03 20:22:53 +0530

    updated hero section

--- components/Home/v3/Hero.tsx
+++ components/Home/v3/Hero.tsx
@@ -40,10 +40,44 @@ export const Hero = (props: Props) => {
   }, [duration]);
 
   return (
-    <div className="h-[calc(100vh-175px)] min-h-[600px]">
-      <ImagesSlider images={backgrounds} overlay={false} initialIndex={currentIndex}>
-        <MaxWidthContainer className="absolute inset-0 flex flex-col justify-center font-normal !py-0 z-50">
-          <div className="w-full md:w-[60%] lg:w-[50%] xl:w-[40%] space-y-4 text-foreground bg-white bg-clip-padding backdrop-filter backdrop-blur-lg md:backdrop-blur-sm bg-opacity-50 px-10 py-16 rounded-[1rem] md:rounded-none md:py-0 md:h-full flex flex-col justify-center">
+    <>
+      <div className="hidden md:block h-[calc(100vh-175px)] min-h-[600px]">
+        <ImagesSlider images={backgrounds} overlay={false} initialIndex={currentIndex}>
+          <MaxWidthContainer className="absolute inset-0 flex flex-col justify-center font-normal !py-0 z-50">
+            <div className="w-full md:w-[60%] lg:w-[50%] xl:w-[40%] space-y-4 text-foreground bg-white bg-clip-padding backdrop-filter backdrop-blur-lg md:backdrop-blur-sm bg-opacity-50 px-10 py-16 rounded-[1rem] md:rounded-none md:py-0 md:h-full flex flex-col justify-center">
+              <div className="text-[2rem] font-light !leading-[1.2] md:text-4xl lg:text-[2.75rem]">
+                We Design
+                <br />
+                <FlipWords words={words} className="font-semibold !text-spj-red" currentIndex={currentIndex} />
+              </div>
+              <p className="mt-2 font-medium md:text-lg">
+                Personalized 3D designs, one-on-one designer collaboration, and handpicked furniture that fits your
+                style & budget.
+              </p>
+              <div className="flex flex-col items-start pt-4 space-y-2 xl:space-x-4 xl:space-y-0 xl:items-center xl:flex-row">
+                <ShinyButton
+                  href="https://designs.spacejoy.com/new-project"
+                  target="_blank"
+                  showRing={false}
+                  className="w-full px-5 py-2.5 xl:w-fit font-semibold"
+                >
+                  Design My Room
+                </ShinyButton>
+              </div>
+              <p>
+                <span className="font-semibold text-spj-red">200,000+</span> beautiful rooms designed and delivered{' '}
+                <span className="font-semibold text-spj-red lg:block 2xl:inline-block">since 2019</span>
+              </p>
+            </div>
+          </MaxWidthContainer>
+        </ImagesSlider>
+      </div>
+      <div className="block md:hidden aspect-[1.375/1]">
+        <ImagesSlider images={backgrounds} overlay={false} initialIndex={currentIndex}>
+          <div />
+        </ImagesSlider>
+        <MaxWidthContainer>
+          <div className="flex flex-col justify-center w-full px-4 py-8 space-y-4 text-foreground">
             <div className="text-[2rem] font-light !leading-[1.2] md:text-4xl lg:text-[2.75rem]">
               We Design
               <br />
@@ -69,7 +103,7 @@ export const Hero = (props: Props) => {
             </p>
           </div>
         </MaxWidthContainer>
-      </ImagesSlider>
-    </div>
+      </div>
+    </>
   );
 };
